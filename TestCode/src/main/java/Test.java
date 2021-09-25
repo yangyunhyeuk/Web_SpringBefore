a567ca4ae1310a96176f37b768f8f4f4328e7405
azz4622@naver.com <azz4622@naver.com>
2021-09-25 22:52:31 +0900

    randomAlphanumeric_library

---
+++ TestCode/src/main/java/Test.java
@@ -0,0 +1,10 @@
+import org.apache.commons.lang3.RandomStringUtils;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(RandomStringUtils.randomAlphanumeric(5));
+
+	}
+}
